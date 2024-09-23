9d25ab09f0f4398f08e5409dd447c62ab08a7742
Laís Brum Menezes <laisbrme@gmail.com>
2024-09-23 19:21:33 -0300

    :sparkles: feat: Respostas do desafio da Vaga Desenvolvedor Back-End

--- questao_5.py
+++
@@ -1,9 +0,0 @@
-def inverter_string(string):
-    str_invertida = ''
-    for caracter in string:
-        str_invertida = caracter + str_invertida
-    return s_invertida
-
-# Testando a função
-print("String original: ", string)
-print("String invertida: ", inverter_string(string))
